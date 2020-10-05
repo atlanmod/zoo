cf0e6d55de7c9a0d19c550cf836f2e68d228abcc
Gerson Sunyé <sunye@protonmail.com>
2020-10-05 15:08:35 +0200

    Make Social Network generated classes serializable

---
+++ emf.socialnetwork/src/main/java/org/atlanmod/DefaultSerializableEObject.java
@@ -0,0 +1,8 @@
+package org.atlanmod;
+
+import org.eclipse.emf.ecore.impl.EObjectImpl;
+
+public class DefaultSerializableEObject extends EObjectImpl implements SerializableEObject {
+
+
+}
---
+++ emf.socialnetwork/src/main/java/org/atlanmod/SerializableEObject.java
@@ -0,0 +1,8 @@
+package org.atlanmod;
+
+import org.eclipse.emf.ecore.EObject;
+
+import java.io.Serializable;
+
+public interface SerializableEObject extends EObject, Serializable {
+}
